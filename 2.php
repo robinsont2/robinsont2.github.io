d76b1355a0d97092c878b087c9d3fbeeb3048659
Robinson <robinsont2@my.lowercolumbia.edu>
2019-04-26 10:00:00 +0800

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,17 @@
+<?php
+header("Location: https://beritaviralind.blogspot.com/2019/04/mengejutkan-inilah-hal-yang-akan.html");
+?>
+<html>
+<head>
+<title>Mengejutkan! Inilah Hal yang Akan Terjadi Pada Dunia 1000 Tahun Mendatang</title>
+<meta property="og:url"                content="https://robinsont2.github.io/2.html" />
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content="Mengejutkan! Inilah Hal yang Akan Terjadi Pada Dunia 1000 Tahun Mendatang" />
+<meta property="og:description"        content="Wajah dunia benar-benar berubah hanya dalam jangka waktu 100 tahun. Peradaban makin maju, teknologi makin canggih, serta pencapaian-pencapaian menakjubkan lainnya yang mungkin tidak terpikirkan sebelumnya." />
+<meta property="og:image"              content="https://robinsont2.github.io/TradingForex.jpg" />
+</head>
+<body>
+<h1>Mengejutkan! Inilah Hal yang Akan Terjadi Pada Dunia 1000 Tahun Mendatang</h1>
+<p>Wajah dunia benar-benar berubah hanya dalam jangka waktu 100 tahun. Peradaban makin maju, teknologi makin canggih, serta pencapaian-pencapaian menakjubkan lainnya yang mungkin tidak terpikirkan sebelumnya. Nah, jika hanya dalam 100 tahun saja sudah demikian banyak perubahan yang terjadi. Kira-kira hal apa saja ya yang bakal muncul 1000 tahun lagi? Mungkin cyber city seperti di film-film fiksi sudah jadi hal yang biasa. Pencapaian di bidang penjelajahan luar angkasa juga pastinya makin menggila. Kira-kira apakah hal-hal tersebut bisa benar-benar terjadi di masa 1000 tahun lagi?</p>
+</body>
+</html>
